5451d89c285113a7a5670707112e669fb14ec7e4
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-06-02 04:40:53 -0700

    create admin management dashboard urls, views

--- dashboard/admin/urls.py
+++ dashboard/admin/urls.py
@@ -4,7 +4,12 @@ from . import views
 app_name = "admin"
 
 urlpatterns = [
+    # Profile Urls
     path("profile/", views.AdminProfileview.as_view(), name="profile"),
     path("profile/edit/", views.AdminProfileEditView.as_view(), name="profile-edit"),
     path("change/password/", views.AdminChangePasswordView.as_view(), name="change-password"),
+
+    # Management Urls
+    path("management/", views.AdminManagementview.as_view(), name="management"),
+    path("users/<int:pk>/detail/", views.AdminUsersDetailview.as_view(), name="users-detail"),
 ]
--- dashboard/admin/views.py
+++ dashboard/admin/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import redirect
-from django.views.generic import View, TemplateView, UpdateView
+from django.views.generic import View, TemplateView, UpdateView, ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth import views as auth_view
-from accounts.models import UserType, Profile
+from accounts.models import UserType, Profile, User
 from dashboard.permissions import HasAdminPermission
 from .forms import AdminProfileEditForm, AdminChangePasswordForm
+from chat.models import ConversationModel
 # Create your views here.
 
 class AdminProfileview(LoginRequiredMixin, HasAdminPermission, TemplateView):
@@ -28,3 +29,15 @@ class AdminChangePasswordView(LoginRequiredMixin, HasAdminPermission, SuccessMes
     form_class = AdminChangePasswordForm
     success_url = reverse_lazy("dashboard:admin:profile")
     success_message = "پسوورد شما بروزرسانی شد"
+
+
+class AdminManagementview(LoginRequiredMixin, HasAdminPermission, ListView):
+    queryset = User.objects.all()
+    template_name = "dashboard/admin/management/user-management.html"
+    context_object_name = "users"
+
+
+class AdminUsersDetailview(LoginRequiredMixin, HasAdminPermission, DetailView):
+    template_name = "dashboard/admin/management/user-detail.html"
+    queryset = User.objects.all()
+    context_object_name ="target_user"
