5c312785b2462bdf285b7086590d2a2a8e013c00
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-06-04 13:26:09 -0700

    update model

--- accounts/models.py
+++ accounts/models.py
@@ -70,8 +70,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_profile")
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
